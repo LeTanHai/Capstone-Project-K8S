cda1a166117f2222940cef1f0046fc96966a9bca
Tan Hai <tanhaile98@gmail.com>
2022-06-10 17:37:54 +0700

    update code

--- jenkins/build_spring_boot_services.groovy
+++ jenkins/build_spring_boot_services.groovy
@@ -39,7 +39,8 @@ pipeline {
         }
         stage('Login into AWS ECR') {
             steps {
-                sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | sudo docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"  
+                sh "aws configure list"
+                sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"  
             }
         }
         stage('Build Cloud Config Server'){

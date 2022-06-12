ce234aa4605de14c5466db68d50dc994ed59a62f
Tan Hai <tanhaile98@gmail.com>
2022-06-12 13:43:26 +0700

    update code

--- jenkins/capstone_project_cicd.groovy
+++ jenkins/capstone_project_cicd.groovy
@@ -27,8 +27,8 @@ pipeline {
                 }
             }
             steps {
-                sh 'cd cloudformation && aws cloudformation create-stack --stack-name capstone-project --template-body file://infrastructure.yml  --parameters file://parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1'
-                sh 'aws cloudformation wait stack-create-complete --region us-east-1 --stack-name capstone-project'
+                sh 'cd cloudformation && aws cloudformation create-stack --stack-name capstoneStack --template-body file://infrastructure.yml  --parameters file://parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1'
+                sh 'aws cloudformation wait stack-create-complete --region us-east-1 --stack-name capstoneStack'
             }
         }
 
